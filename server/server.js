c28ab99775a1a063ff6fac04885816de552947c3
Samuel Bodenhemier <swb79@nau.edu>
2025-11-04 17:49:33 -0700

    Initialize project

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
